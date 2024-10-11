a561c8f2b1a9b26fd3021f77f6b3430ed8da7fc2
Daniel Bittman <danielbittman1@gmail.com>
2024-10-11 15:16:33 -0700

    Update status.

--- instant-distance/src/lib.rs
+++ instant-distance/src/lib.rs
@@ -546,7 +546,7 @@ impl<'a, P: Point> Construction<'a, P> {
 
         let value = self.done.fetch_add(1, atomic::Ordering::Relaxed);
         if value % 1000 == 0 {
-            print!(
+            eprint!(
                 "building index, point {} / {}, in layer {}              \r",
                 value,
                 self.points.len(),
