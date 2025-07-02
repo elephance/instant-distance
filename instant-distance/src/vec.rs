fafde416f3497ad6844d234a86c2ea3387209e17
Allen Aboytes <allen@elephancememory.com>
2025-07-02 15:34:30 -0700

    Simple iterator implementation

--- instant-distance/src/vec.rs
+++ instant-distance/src/vec.rs
@@ -1,45 +1,66 @@
 // pub use std::vec::Vec;
 use std::marker::PhantomData;
 
+use rayon::iter::{FromParallelIterator, ParallelIterator};
+
 use crate::{Layer, NearestIter, Point, PointId, PointMgr, UpperNode, ZeroNode};
 
 pub type Vec<T> = SegmentedVector<T>;
 
 // TODO: implement Clone
-#[derive(Clone)]
-pub(crate) struct SegmentedVector<T> {
+#[derive(Debug, Clone, PartialEq)]
+pub struct SegmentedVector<T> {
     length: usize,
-    inner: std::vec::Vec<T>,
-    // _data: PhantomData<T>,
+    segment_size: usize,
+    segments: std::vec::Vec<SegmentArray<T>>,
 }
 
 impl<T> SegmentedVector<T> {
-    pub fn new() -> Self { todo!() }
+    pub const DEFAULT_SEGMENT_SIZE: usize = 1024 * 1024 * 1024;
+    pub fn new() -> Self {
+        Self { 
+            length: 0,
+            segment_size: Self::DEFAULT_SEGMENT_SIZE,
+            segments: std::vec::Vec::new(),
+        }
+    }
 
-    pub fn len(&self) -> usize { todo!() }
+    /// length in the number of elements
+    pub fn len(&self) -> usize {
+        self.length
+    }
 
     pub fn iter(&self) -> Iter<'_, T> {
+        // self.segments.iter()
         todo!()
     }
     
-    // fn segments(&self) -> SegmentIter {
-    //     // self.inner.iter()
-    //     todo!()
-    // }
+    pub fn segment_size(&self) -> usize {
+        self.segment_size
+    }
 
-    fn num_segments(&self) -> usize {
-        todo!()
+    fn segments(&self) -> std::slice::Iter<SegmentArray<T>> {
+        self.segments.iter()
+    }
+
+    // TODO: should we hide this?
+    pub fn num_segments(&self) -> usize {
+        self.segments.len()
     }
 
     #[inline]
-    pub fn get(&self, _idx: usize) -> Option<&T> {
-        todo!()
-        // if std::intrinsics::likely(idx < self.length) {
-            // Ok(self.get_slice_containing_unchecked(idx).get(rel))
-        // } else {
-        //     Err("index out of bounds")
-        // }
+    pub fn get(&self, idx: usize) -> Option<&T> {
+        if std::intrinsics::likely(idx < self.length) {
+            Ok(self.get_slice_containing_unchecked(idx).get(rel))
+        } else {
+            Err("index out of bounds")
+        }
 
+    }
+
+    pub fn push(&mut self, value: T) {
+        // self.segments.push(value);
+        todo!()
     }
 
 }
@@ -69,7 +90,9 @@ impl<'a, T> Iterator for Iter<'a, T> {
 }
 
 // TODO: maybe not change all vec to be segmented vec ...
+#[derive(Debug, Clone, PartialEq)]
 struct SegmentArray<T> {
+    // TODO: do we make this a vec with the option to have a different allocator?
     _data: PhantomData<T>,
 }
 
@@ -116,5 +139,33 @@ impl<'a, P: Point> PointMgr<'a, P> for &'a SegmentedVector<P> {
 
     fn num_vectors(&self) -> usize {
         self.len()
+    }
+}
+
+impl<T> FromIterator<T> for SegmentedVector<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut vec = SegmentedVector::new();
+        iter.into_iter().for_each(|item| vec.push(item));
+        vec
+    }
+}
+
+impl<T: Send> FromParallelIterator<T> for SegmentedVector<T> {
+    fn from_par_iter<I>(par_iter: I) -> Self
+        where
+            I: rayon::prelude::IntoParallelIterator<Item = T> 
+    {
+        // todo!()
+        // could use a variety of methods
+        // - use collect_vec_list -> copy each individually
+        // - fold/fold_with (does not preserve order)
+        // - for_each (does not preserve order)
+        // - map items with index, then insert in specified place?
+        let ordered_list = par_iter.into_par_iter().collect_vec_list();
+        ordered_list.into_iter().flatten().collect()
+ 
+        // TODO: better impl, use reserve method, add insert as well
+
+        // not the best since it probs uses a ton of memory...
     }
 }
